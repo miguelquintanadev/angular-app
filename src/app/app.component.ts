3d765fcf2d0f2a4c65e9918672705ae432a09680
Miguel Quintana <miguelquintanadev@gmail.com>
2024-06-04 22:51:40 -0400

    Prueba 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title: string = 'Hola mundo Angular desde componente!';
+
+  users: string[] = ['Pepe', 'Maria', 'Juan', 'Andres'];
+
+  visible: boolean = false;
+
+  setVisible(): void {
+    this.visible = this.visible? false : true;
+    console.log('Hemos hecho clic en setVisible');
+  }
 }
