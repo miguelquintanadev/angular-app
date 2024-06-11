61fbb7138faee84e2d6489d899a852a6dc1591f1
Miguel Quintana <miguelquintanadev@gmail.com>
2024-06-11 01:13:31 -0400

    Son to father

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CounterComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title: string = 'Hola mundo Angular desde componente!';
+
+  subTitle = 'Contador con estado de sesión';
 
   users: string[] = ['Pepe', 'Maria', 'Juan', 'Andres'];
 
   visible: boolean = false;
 
+  counter: number = 0;
+
+  ngOnInit(): void {
+    this.counter = parseInt(sessionStorage.getItem('counter')!) || 0;
+  }
+
   setVisible(): void {
     this.visible = this.visible? false : true;
     console.log('Hemos hecho clic en setVisible');
+  }
+
+  setCounter(counter:number):void {
+    this.counter = counter;
   }
 }
